169c4e925d0197fdf1658b6a8a7dabe2cacec40e
InJung Chung <mu29gl@gmail.com>
2016-03-14 23:22:51 +0900

    설정 파일 추가

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = 'secretkey'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
