ee4d82395cf340beb5eb60cd573b4132d62f6160
InJung Chung <mu29gl@gmail.com>
2016-03-14 22:32:47 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+#-*- coding: utf-8 -*-
+
+from flask import *
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
+def init():
+    for module in PAGES:
+        __import__(module)
